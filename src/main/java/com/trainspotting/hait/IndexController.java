c97ca43ab1d130ed6eab59b6d870c8fdd04f87e8
reversee-mermaid <reversee.mermaid@gmail.com>
2021-03-15 23:12:07 +0900

    Set IndexController mapping

--- src/main/java/com/trainspotting/hait/IndexController.java
+++ src/main/java/com/trainspotting/hait/IndexController.java
@@ -2,13 +2,16 @@ package com.trainspotting.hait;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = {"/home/**", "/owner/**", "/customer/**", "/admin/**"}, method = RequestMethod.GET)
 public class IndexController {
 	
-	@GetMapping("/errors")
-	public String error() {
+	@GetMapping
+	public String index() {
+		System.out.println("index controller!");
 		return "index";
 	}
-	
 }
