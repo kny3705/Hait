d87540052837cf71782eac2866d3de58fc8b52b8
reversee-mermaid <reversee.mermaid@gmail.com>
2021-03-31 03:04:25 +0900

    Remove useless code & Fix sql

--- src/main/java/com/trainspotting/hait/admin/AdminService.java
+++ src/main/java/com/trainspotting/hait/admin/AdminService.java
@@ -1,11 +1,5 @@
 package com.trainspotting.hait.admin;
 
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-import java.util.UUID;
-
-import javax.mail.MessagingException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,10 +7,6 @@ import org.springframework.stereotype.Service;
 import com.trainspotting.hait.exception.LoginFailedException;
 import com.trainspotting.hait.jwt.JwtProvider;
 import com.trainspotting.hait.model.AdminEntity;
-import com.trainspotting.hait.model.ApplicationDTO;
-import com.trainspotting.hait.model.ApplicationEntity;
-import com.trainspotting.hait.model.OwnerEntity;
-import com.trainspotting.hait.util.MailUtil;
 
 @Service
 public class AdminService {
@@ -29,9 +19,6 @@ public class AdminService {
 	
 	@Autowired
 	private JwtProvider jwtProvider;
-	
-	@Autowired
-	private MailUtil mail;
 	
 	public String login(AdminEntity param) {
 		
--- src/main/java/com/trainspotting/hait/application/ApplicationController.java
+++ src/main/java/com/trainspotting/hait/application/ApplicationController.java
@@ -1,8 +1,6 @@
 package com.trainspotting.hait.application;
 
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.mail.MessagingException;
 
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trainspotting.hait.ResponseBody;
